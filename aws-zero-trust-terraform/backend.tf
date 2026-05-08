6a494cc743c119d423e38a3ee620ed8d302dc3a5
Marlon Garcia <cloud.veteran@proton.me>
2026-05-08 18:06:31 -0400

    update

--- aws-zero-trust-terraform/backend.tf
+++ aws-zero-trust-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket-here"
-    key            = "landing-zone/terraform.tfstate"
+    bucket         = "terraform-state-bucket-mag0508 "
+    key            = "zero-trust/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile   = true
     encrypt        = true
