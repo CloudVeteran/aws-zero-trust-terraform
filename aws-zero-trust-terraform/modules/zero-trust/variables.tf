eccca61b628e7978349a089f0358cba4a192aac1
Marlon Garcia <cloud.veteran@proton.me>
2026-05-08 17:49:59 -0400

    Add variables for project configuration in Terraform

---
+++ aws-zero-trust-terraform/modules/zero-trust/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  type    = string
+  default = "zero-trust"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
